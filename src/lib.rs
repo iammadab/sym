ab167b1e9ab6153b756ded18f107ec4e529ea0f7
Wisdom Ogwu <iammadab@gmail.com>
2024-11-06 17:13:18 +0100

    implement negation + subtraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Mul, Sub};
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Atom {
     Variable(&'static str),
     Integer(usize),
@@ -26,6 +26,17 @@ impl Add for Atom {
     }
 }
 
+impl Sub for Atom {
+    type Output = Expression;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Expression::Add(
+            Box::new(self.into()),
+            Box::new(Expression::Neg(Box::new(rhs.into()))),
+        )
+    }
+}
+
 impl Mul for Atom {
     type Output = Expression;
 
@@ -34,11 +45,18 @@ impl Mul for Atom {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Expression {
     Atom(Atom),
+    Neg(Box<Expression>),
     Add(Box<Expression>, Box<Expression>),
     Mul(Box<Expression>, Box<Expression>),
+}
+
+impl Expression {
+    fn neg(&self) -> Expression {
+        Expression::Neg(Box::new(self.clone()))
+    }
 }
 
 impl Add for Expression {
@@ -57,6 +75,25 @@ impl Add<Atom> for Expression {
     }
 }
 
+impl Sub for Expression {
+    type Output = Expression;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Expression::Add(Box::new(self), Box::new(Expression::Neg(Box::new(rhs))))
+    }
+}
+
+impl Sub<Atom> for Expression {
+    type Output = Expression;
+
+    fn sub(self, rhs: Atom) -> Self::Output {
+        Expression::Add(
+            Box::new(self),
+            Box::new(Expression::Neg(Box::new(rhs.into()))),
+        )
+    }
+}
+
 impl Mul for Expression {
     type Output = Expression;
 
@@ -70,5 +107,19 @@ impl Mul<Atom> for Expression {
 
     fn mul(self, rhs: Atom) -> Self::Output {
         Expression::Mul(Box::new(self), Box::new(rhs.into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Atom;
+
+    #[test]
+    fn fake_test() {
+        let x = Atom::Variable("x");
+        let y = x + Atom::Integer(1);
+        let z = y * Atom::Integer(2);
+        let m = z - Atom::Integer(3);
+        dbg!(m);
     }
 }
