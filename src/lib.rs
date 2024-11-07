83adb1544fada543ef7cb93f6bd768515dea8bcb
Wisdom Ogwu <iammadab@gmail.com>
2024-11-07 14:39:14 +0100

    simplify atom add, sub and mul definitions via macros

--- src/lib.rs
+++ src/lib.rs
@@ -58,56 +58,57 @@ impl From<&Atom> for Expression {
     }
 }
 
-impl Add for &Atom {
-    type Output = Expression;
+macro_rules! impl_add {
+    ($lhs:ty, $rhs:ty) => {
+        impl Add<$rhs> for $lhs {
+            type Output = Expression;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        Expression::Add(Box::new(self.into()), Box::new(rhs.into()))
-    }
+            fn add(self, rhs: $rhs) -> Self::Output {
+                Expression::Add(Box::new(self.into()), Box::new(rhs.into()))
+            }
+        }
+    };
 }
 
-impl Add for Atom {
-    type Output = Expression;
+macro_rules! impl_sub {
+    ($lhs:ty, $rhs:ty) => {
+        impl Sub<$rhs> for $lhs {
+            type Output = Expression;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        Expression::Add(Box::new(self.into()), Box::new(rhs.into()))
-    }
+            fn sub(self, rhs: $rhs) -> Self::Output {
+                Expression::Add(
+                    Box::new(self.into()),
+                    Box::new(Expression::Neg(Box::new(rhs.into()))),
+                )
+            }
+        }
+    };
 }
 
-impl Add<&Atom> for Atom {
-    type Output = Expression;
+macro_rules! impl_mul {
+    ($lhs:ty, $rhs:ty) => {
+        impl Mul<$rhs> for $lhs {
+            type Output = Expression;
 
-    fn add(self, rhs: &Atom) -> Self::Output {
-        Expression::Add(Box::new(self.into()), Box::new(rhs.into()))
-    }
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                Expression::Mul(Box::new(self.into()), Box::new(rhs.into()))
+            }
+        }
+    };
 }
 
-impl Add<Atom> for &Atom {
-    type Output = Expression;
-
-    fn add(self, rhs: Atom) -> Self::Output {
-        Expression::Add(Box::new(self.into()), Box::new(rhs.into()))
-    }
+macro_rules! impl_type_combination {
+    ($impl_name:ident, $type_name:ty) => {
+        $impl_name!($type_name, $type_name);
+        $impl_name!(&$type_name, &$type_name);
+        $impl_name!($type_name, &$type_name);
+        $impl_name!(&$type_name, $type_name);
+    };
 }
 
-impl Sub for &Atom {
-    type Output = Expression;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Expression::Add(
-            Box::new(self.into()),
-            Box::new(Expression::Neg(Box::new(rhs.into()))),
-        )
-    }
-}
-
-impl Mul for &Atom {
-    type Output = Expression;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        Expression::Mul(Box::new(self.into()), Box::new(rhs.into()))
-    }
-}
+impl_type_combination!(impl_add, Atom);
+impl_type_combination!(impl_sub, Atom);
+impl_type_combination!(impl_mul, Atom);
 
 #[derive(Clone, Debug, PartialEq)]
 enum Expression {
