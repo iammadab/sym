234e8d7213c66a6795539e2d72b36369233d884d
Wisdom Ogwu <iammadab@gmail.com>
2024-11-09 12:13:02 +0100

    add and test inv simplification rules

--- src/simplify.rs
+++ src/simplify.rs
@@ -1,12 +1,11 @@
 use crate::Expression;
 
 pub(crate) fn simplify_neg(expression: Expression) -> Expression {
+    let child = expression.children()[0];
+
     // Substitution Rules
     // 1. Neg(Neg(x)) => x
     // 2. Neg(integer) => -1 * integer
-
-    let child = expression.children()[0];
-
     match child {
         Expression::Neg(inner_expr) => (**inner_expr).clone(),
         Expression::Integer(val) => Expression::Integer(-1 * val),
@@ -15,7 +14,14 @@ pub(crate) fn simplify_neg(expression: Expression) -> Expression {
 }
 
 pub(crate) fn simplify_inv(expression: Expression) -> Expression {
-    todo!()
+    let child = expression.children()[0];
+
+    // Substitution Rules
+    // Inv(Inv(x)) => x
+    match child {
+        Expression::Inv(inner_expr) => (**inner_expr).clone(),
+        _ => expression,
+    }
 }
 pub(crate) fn simplify_add(expression: Expression) -> Expression {
     todo!()
@@ -47,6 +53,27 @@ mod tests {
                 .simplify()
                 .to_string(),
             "-2"
+        );
+
+        // Neg(-2) = 2
+        assert_eq!(
+            Expression::Neg(Box::new(Expression::Integer(-2)))
+                .simplify()
+                .to_string(),
+            "2"
+        );
+    }
+
+    #[test]
+    fn test_inverse_simplification() {
+        // Inv(Inv(a)) = a
+        assert_eq!(
+            Expression::Inv(Box::new(Expression::Inv(Box::new(Expression::Variable(
+                "a"
+            )))))
+            .simplify()
+            .to_string(),
+            "a"
         );
     }
 }
