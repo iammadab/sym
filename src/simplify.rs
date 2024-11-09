b73420a293d81f390389a0c7ceddb3bf59b94176
Wisdom Ogwu <iammadab@gmail.com>
2024-11-09 12:08:19 +0100

    test negation simplification rules

--- src/simplify.rs
+++ src/simplify.rs
@@ -1,33 +1,52 @@
 use crate::Expression;
 
-pub fn simplify_neg(expression: Expression) -> Expression {
+pub(crate) fn simplify_neg(expression: Expression) -> Expression {
     // Substitution Rules
     // 1. Neg(Neg(x)) => x
     // 2. Neg(integer) => -1 * integer
 
-    // match expression {
-    //     Expression::Integer(val) => Expression::Integer(-1 * val),
-    //     Expression::Variable()
-    // }
+    let child = expression.children()[0];
 
+    match child {
+        Expression::Neg(inner_expr) => (**inner_expr).clone(),
+        Expression::Integer(val) => Expression::Integer(-1 * val),
+        _ => expression,
+    }
+}
+
+pub(crate) fn simplify_inv(expression: Expression) -> Expression {
+    todo!()
+}
+pub(crate) fn simplify_add(expression: Expression) -> Expression {
     todo!()
 }
 
-pub fn simplify_inv(expression: Expression) -> Expression {
-    todo!()
-}
-pub fn simplify_add(expression: Expression) -> Expression {
-    todo!()
-}
-
-pub fn simplify_mul(expression: Expression) -> Expression {
+pub(crate) fn simplify_mul(expression: Expression) -> Expression {
     todo!()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::Expression;
+
     #[test]
     fn test_negation_simplification() {
-        todo!()
+        // Neg(Neg(a)) = a
+        assert_eq!(
+            Expression::Neg(Box::new(Expression::Neg(Box::new(Expression::Variable(
+                "a"
+            )))))
+            .simplify()
+            .to_string(),
+            "a"
+        );
+
+        // Neg(2) = -2
+        assert_eq!(
+            Expression::Neg(Box::new(Expression::Integer(2)))
+                .simplify()
+                .to_string(),
+            "-2"
+        );
     }
 }
