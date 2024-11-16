71ce9664400751b4142db01298c3421a08fefc77
Wisdom Ogwu <iammadab@gmail.com>
2024-11-16 12:19:39 +0100

    add test for exponentiation simplification

--- src/simplify.rs
+++ src/simplify.rs
@@ -201,6 +201,20 @@ mod tests {
     }
 
     #[test]
+    fn test_exponentiation_simplification() {
+        let x = Expression::Variable("x".to_string());
+
+        let x_to_0 = x.pow(&Expression::Integer(0)).simplify();
+        assert_eq!(x_to_0, Expression::Integer(1));
+
+        let x_to_1 = x.pow(&Expression::Integer(1)).simplify();
+        assert_eq!(x_to_1.to_string(), "x");
+
+        let zero_to_x = Expression::Integer(0).pow(&x).simplify();
+        assert_eq!(zero_to_x.to_string(), "0");
+    }
+
+    #[test]
     fn test_add_simplification() {
         // 2 integers
         assert_eq!(
