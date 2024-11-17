c40e9ad9501146a49816c7fde89c1b17dfa48359
Wisdom Ogwu <iammadab@gmail.com>
2024-11-17 19:15:23 +0100

    test multiplication flatenning

--- src/simplify/mul.rs
+++ src/simplify/mul.rs
@@ -17,6 +17,26 @@ mod tests {
 
     #[test]
     fn test_mul_simplification() {
+        // flatten mul expressions
+        assert_eq!(
+            Expression::Mul(vec![
+                Expression::Variable("x".to_string()),
+                Expression::Mul(vec![
+                    Expression::Variable("z".to_string()),
+                    Expression::Variable("y".to_string())
+                ])
+            ])
+            .simplify(),
+            Expression::Mul(vec![
+                Expression::Variable("x".to_string()),
+                Expression::Variable("y".to_string()),
+                Expression::Variable("z".to_string())
+            ])
+        );
+    }
+
+    #[test]
+    fn test_old_mul_simplification() {
         // Integers mixed with variables
         assert_eq!(
             Expression::Mul(vec![
