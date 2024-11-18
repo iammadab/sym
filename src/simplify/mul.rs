fd2ac186e5d44144c1bed3461777ecb687e2629b
Wisdom Ogwu <iammadab@gmail.com>
2024-11-18 07:54:21 +0100

    remove old test

--- src/simplify/mul.rs
+++ src/simplify/mul.rs
@@ -141,42 +141,4 @@ mod tests {
             )
         )
     }
-
-    #[test]
-    fn test_old_mul_simplification() {
-        // Integers mixed with variables
-        assert_eq!(
-            Expression::Mul(vec![
-                Expression::Integer(3),
-                Expression::Variable("x".to_string()),
-                Expression::Integer(4),
-                Expression::Variable("y".to_string())
-            ])
-            .simplify()
-            .to_string(),
-            "12xy"
-        );
-
-        assert_eq!(
-            Expression::Mul(vec![
-                Expression::Mul(vec![
-                    Expression::Variable("a".to_string()),
-                    Expression::Integer(2)
-                ]),
-                Expression::Mul(vec![
-                    Expression::Integer(-2),
-                    Expression::Variable("b".to_string()),
-                    Expression::Integer(2)
-                ]),
-                Expression::Mul(vec![
-                    Expression::Integer(2),
-                    Expression::Variable("c".to_string()),
-                    Expression::Integer(2)
-                ]),
-            ])
-            .simplify()
-            .to_string(),
-            "-32abc"
-        );
-    }
 }
