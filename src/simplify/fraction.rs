6ea39edcaf083f9ec69f8448aed19981cb889857
Wisdom Ogwu <iammadab@gmail.com>
2024-11-18 15:46:39 +0100

    add failing test for fraction simplification

--- src/simplify/fraction.rs
+++ src/simplify/fraction.rs
@@ -4,3 +4,40 @@ pub(crate) fn simplify_fraction(expression: Expression) -> Expression {
     let (numerator, denominator) = expression.decompose_fraction().unwrap();
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Expression;
+
+    #[test]
+    fn test_fraction_simplification() {
+        assert_eq!(
+            Expression::Fraction(1, 1).simplify(),
+            Expression::Fraction(1, 1)
+        );
+        assert_eq!(
+            Expression::Fraction(1, -1).simplify(),
+            Expression::Fraction(-1, 1)
+        );
+        assert_eq!(
+            Expression::Fraction(-1, -1).simplify(),
+            Expression::Fraction(1, 1)
+        );
+        assert_eq!(
+            Expression::Fraction(2, 2).simplify(),
+            Expression::Fraction(1, 1)
+        );
+        assert_eq!(
+            Expression::Fraction(4, 2).simplify(),
+            Expression::Fraction(2, 1)
+        );
+        assert_eq!(
+            Expression::Fraction(2, 4).simplify(),
+            Expression::Fraction(1, 2)
+        );
+        assert_eq!(
+            Expression::Fraction(2, 3).simplify(),
+            Expression::Fraction(2, 3)
+        );
+    }
+}
